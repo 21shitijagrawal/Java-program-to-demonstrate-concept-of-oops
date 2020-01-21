4d42f2c720b926e8b8fe9514f34b9ca44efc3ff1
21shitijagrawal <44740279+21shitijagrawal@users.noreply.github.com>
2020-01-21 19:54:16 +0530

    Update OOPS.java

--- OOPS.java
+++ OOPS.java
@@ -1,10 +1,14 @@
 /* See the Description for more infromation. */
 import java.util.Scanner;
-class Person{
+interface Data{
+	void get();
+	void show();
+}
+class Person implements Data{
 	protected String name;
 	protected String address;
 	protected int age;
-	void get(){
+	public void get(){
 		Scanner s = new Scanner(System.in);
 		System.out.print("\nEnter Name : ");
 		this.name = s.next();
@@ -13,21 +17,21 @@ class Person{
 		System.out.print("\nEnter Address : ");
 		this.address = s.next();
 	}
-	void get(String name, int  age, String address){
+	public void get(String name, int age, String address){ // Method overloading.
 		this.name = name;
 		this.age = age;
 		this.address = address;
 	}
-	void show(){
+	public void show(){
 		System.out.println("Name is : " + this.name);
 		System.out.println("Age is : " + this.age);
 		System.out.println("Address : " + this.address);
 	}
 }
 class Employee extends Person {
-	private int employeeId;
+	private int employeeId; 
 	private double salary;
-	void get(){
+	public void get(){ // Runtime Polymorphism :  overwriting method of super class.
 		Scanner s = new Scanner(System.in);
 		super.get();
 		System.out.print("\nEnter the Employee Id : ");
@@ -35,14 +39,21 @@ class Employee extends Person {
 		System.out.print("\nEnter the Employee Salary : ");
 		this.salary = s.nextDouble();
 	}
-	void show(){
+	private double getSalary(){  // Encapsulation
+		return this.salary;
+	}
+	public void show(){
 		super.show();
 		System.out.println("Employee Id : " + this.employeeId);
-		System.out.println("Employee Salary : " + this.salary);
+		System.out.println("Employee Salary : " + getSalary());
 	}
 }
 class OOPS{
 	public static void main(String[] args){
+		Person p = new Person();
+		p.get("Person", 16, "ABCB CDJ");
+		p.show();
+
 		Employee e = new Employee();
 		e.get();
 		e.show();
